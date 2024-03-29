865081c92552db6cb0fba00fe086a65d30fbba66
Dannymizrachi <danny.mizrachi11@gmail.com>
2024-03-29 22:08:42 +0300

    start tic tac toe game board

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const Player = ({ initialName, symbol }) => {
+	const [isEditing, setIsEditing] = useState(false);
+	const [playerName, setPlayerName] = useState(initialName);
+
+	const hangleEditClick = () => {
+		setIsEditing((editing) => !editing);
+	};
+
+	const handleChangeName = (e) => {
+		setPlayerName(e.target.value);
+	};
+
+	return (
+		<li>
+			<span className='player'>
+				{isEditing ? <input required value={playerName} onChange={handleChangeName} /> : <span className='player-name'>{playerName}</span>}
+				<span className='player-symbol'>{symbol}</span>
+			</span>
+			<button onClick={hangleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+		</li>
+	);
+};
+
+export default Player;
